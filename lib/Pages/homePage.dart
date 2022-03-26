3695f1f85bd15a06f668800b4cc3565fb8117d5d
qi1n <44217604+qi1n@users.noreply.github.com>
2022-03-26 14:52:26 +0100

    crop image(without async)

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -105,10 +105,40 @@ class _HomePageState extends State<HomePage> {
       image = await ImagePicker().pickImage(source: ImageSource.gallery);
     }
     if (image == null) return;
-    setState(() {
-      imageFile = File(image.path);
-      imageData = base64Encode(imageFile.readAsBytesSync());
-    });
+    File? croppedFile = await ImageCropper().cropImage(sourcePath: image.path,
+        aspectRatioPresets: Platform.isAndroid
+            ? [
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio16x9
+        ]
+            : [
+          CropAspectRatioPreset.original,
+          CropAspectRatioPreset.square,
+          CropAspectRatioPreset.ratio3x2,
+          CropAspectRatioPreset.ratio4x3,
+          CropAspectRatioPreset.ratio5x3,
+          CropAspectRatioPreset.ratio5x4,
+          CropAspectRatioPreset.ratio7x5,
+          CropAspectRatioPreset.ratio16x9
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          title: 'Cropper',
+        ));
+    if(croppedFile != null) {
+      setState(() {
+        imageFile = File(croppedFile.path);
+        imageData = base64Encode(imageFile.readAsBytesSync());
+      });
+    }
     doUpload();
   }
 
@@ -178,29 +208,29 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         floatingActionButton:
             Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-          FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {},
-            heroTag: null,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            backgroundColor: Color.fromRGBO(178,207, 135, 0.8),
-            child: Icon(Icons.photo_album),
-            onPressed: () => pickImage(false),
-            heroTag: null,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            backgroundColor: Color.fromRGBO(178,207, 135, 0.8),
-            child: Icon(Icons.camera_alt),
-            onPressed: () => pickImage(true),
-            heroTag: null,
-          )
+              FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: () {},
+                heroTag: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FloatingActionButton(
+                backgroundColor: Color.fromRGBO(178,207, 135, 0.8),
+                child: Icon(Icons.photo_album),
+                onPressed: () => pickImage(false),
+                heroTag: null,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FloatingActionButton(
+                backgroundColor: Color.fromRGBO(178,207, 135, 0.8),
+                child: Icon(Icons.camera_alt),
+                onPressed: () => pickImage(true),
+                heroTag: null,
+              )
         ]),
         body: new Stack(
           children: <Widget>[
